d239b21544b2e7805c9de49f1f8bed4b4951ef41
theapi <petermclarke@gmail.com>
2017-02-26 17:06:40 +0000

    Now talking to the cupboard

--- hello_world/src/index.js
+++ hello_world/src/index.js
@@ -19,33 +19,20 @@ var handlers = {
     },
 
     'GetCupboardIntent': function () {
-        // var speechOutput = getCupboardTemperature();
-        // this.emit(':tell', speechOutput);
-        //this.emit(':tell', 'I don\'t know the cupboard temperature yet.');
-
-        var msg = getCupboardTemperature(function(speechOutput) {
-                  console.log(speechOutput);
-                  return speechOutput;
-          //this.emit(':tell', speechOutput);
+        var that = this;
+        var speechOutput = 'Request test';
+        request.get(url(), function(error, response, body) {
+            console.log(body);
+            var d = JSON.parse(body);
+            if (d.temperature) {
+                 that.emit(':tell', "The temperature in the cupboard is " + d.temperature);
+             } else {
+                 that.emit(':tell', "Sorry, I could not get the temperature in the cupboard");
+             }
         });
-        this.emit(':tell', msg);
     }
  };
 
  function url() {
      return "http://io.theapi.co.uk/cupboard/v0.1/index.json";
- }
-
- function getCupboardTemperature(callback) {
-   callback("Sorry, I could not get the temperature in the cupboard");
-    //  request.get(url(), function(error, response, body) {
-    //      var d = JSON.parse(body)
-    //      var temperature = d.temperature
-    //      if (temperature > 0) {
-    //          callback("The temperature in the cupboard is " + temperature);
-    //      } else {
-    //          callback("Sorry, I could not get the temperature in the cupboard");
-    //      }
-     //
-    //  })
  }
