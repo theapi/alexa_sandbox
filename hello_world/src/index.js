9796c6cbb2fafe1cdc3589dae6e8af7b349f681d
theapi <petermclarke@gmail.com>
2017-02-26 14:10:33 +0000

    Experimenting with url requests

--- hello_world/src/index.js
+++ hello_world/src/index.js
@@ -1,7 +1,12 @@
 var Alexa = require('alexa-sdk');
+var request = require("request");
+var APP_ID = 'theapi_hello_world';
 
-exports.handler = function(event, context, callback){
+exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
 };
 
 var handlers = {
@@ -11,11 +16,36 @@ var handlers = {
 
     'HelloWorldIntent': function () {
         this.emit(':tell', 'Hello, my name is Alexa');
+    },
+
+    'GetCupboardIntent': function () {
+        // var speechOutput = getCupboardTemperature();
+        // this.emit(':tell', speechOutput);
+        //this.emit(':tell', 'I don\'t know the cupboard temperature yet.');
+
+        var msg = getCupboardTemperature(function(speechOutput) {
+                  console.log(speechOutput);
+                  return speechOutput;
+          //this.emit(':tell', speechOutput);
+        });
+        this.emit(':tell', msg);
     }
  };
 
- exports.handler = function(event, context, callback) {
-     var alexa = Alexa.handler(event, context);
-     alexa.registerHandlers(handlers);
-     alexa.execute();
- };
+ function url() {
+     return "http://io.theapi.co.uk/cupboard/v0.1/index.json";
+ }
+
+ function getCupboardTemperature(callback) {
+   callback("Sorry, I could not get the temperature in the cupboard");
+    //  request.get(url(), function(error, response, body) {
+    //      var d = JSON.parse(body)
+    //      var temperature = d.temperature
+    //      if (temperature > 0) {
+    //          callback("The temperature in the cupboard is " + temperature);
+    //      } else {
+    //          callback("Sorry, I could not get the temperature in the cupboard");
+    //      }
+     //
+    //  })
+ }
