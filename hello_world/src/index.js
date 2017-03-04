87960deac330ca360804c7f926ed647d8f1268aa
Peter Clarke <petermclarke@gmail.com>
2017-03-04 18:18:20 +0000

    Set the app_id

--- hello_world/src/index.js
+++ hello_world/src/index.js
@@ -5,7 +5,7 @@ var APP_ID = config.app_id;
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
+    alexa.appId = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
